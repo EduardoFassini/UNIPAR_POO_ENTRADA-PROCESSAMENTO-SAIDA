69713437efdded6fd1ac1665ec4138a791b5d7e5
EduardoFassini <151883534+EduardoFassini@users.noreply.github.com>
2024-03-05 12:51:44 -0300

    Create Atividade3.java

---
+++ Atividade3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        double nota1;
+        double nota2;
+        
+        System.out.println("Informe a primeira nota:");
+        nota1 = scanner.nextDouble();
+        
+        System.out.println("Informe a segunda nota:");
+        nota2 = scanner.nextDouble();
+        
+        double media = (nota1 + nota2) / 2;
+        
+        System.out.println("A media e: " + media);
+        
+        scanner.close();
+    }
+}
