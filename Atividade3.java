6b56390861cfd40ab4610b971bea70195706c696
EduardoFassini <151883534+EduardoFassini@users.noreply.github.com>
2024-03-06 15:32:20 -0300

    Update Atividade3.java

--- Atividade3.java
+++ Atividade3.java
@@ -18,6 +18,12 @@ class HelloWorld {
         
         System.out.println("A media e: " + media);
         
+        if(media>=6){
+            System.out.println("Aprovado.");
+        }else{
+            System.out.println("Reprovado.");
+        }
+        
         scanner.close();
     }
 }
